05637bf114cc0ea396d183472e15ecdcbb578764
smj2021 <mjschwartz515@gmail.com>
2021-12-14 23:17:48 -0500

    add projects and bio

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://github.com/smj2021/cleanfolio'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+      Created By Michael Schwartz
     </a>
   </footer>
 )
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://schwartzbewithyou.com/',
+  title: 'MS.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Michael Schwartz',
+  role: 'Full Stack Software Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    "I'm a problem solver; driven to create stunning, functional, enjoyable user interactions. I leverage my past as an intelligence officer, diplomat, and consulting manager to elicit user stories, then wireframe and map requirements to industry best practices using my training in human-centered design. I love to collaborate, share ideas, and work holistically to build resilient, scalable, engaging solutions to client challenges.",
+  resume: 'https://schwartzbewithyou.com/wp-content/uploads/2021/12/Michael-Schwartz-CV.pdf',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/schwartzbewithyou/',
+    github: 'https://github.com/smj2021',
   },
 }
 
@@ -21,27 +21,27 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'PUMP',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
+      'PUMP is a Python and Django web application which allows users to securely create and manage fitness routines and nutrition plans from their desktop or mobile device. Data storage is performed by the integration of a PostgreSQL database.',
+    stack: ['Python', 'Django', 'PostgreSQL','HTML5', 'CSS3', 'Bootstrap'],
+    sourceCode: 'https://github.com/fattony26/workout-city',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 2',
+    name: 'Anime City',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
+      'Anime City is a full-stack (MERN) web application utilizing the Jikan API to allow users to query and create lists of their favorite anime and manga series, as well as connecting them to platforms on which they are able to watch the items on their lists.',
+    stack: ['JavaScript', 'ExpressJS', 'Node.js', 'React', 'MongoDB', 'HTML5', 'CSS3'],
+    sourceCode: 'https://github.com/smj2021/anime_city',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 3',
+    name: "Chef's Corner",
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
+      "Chef's Corner is a full-stack (MEN) web application built allowing users to add recipes to a database and build individualized cookbooks for storing their favorite recipes.",
+    stack: ['JavaScript', 'ExpressJS', 'Node.js', 'MongoDB', 'HTML5', 'CSS3'],
+    sourceCode: 'https://github.com/smj2021/foodbook',
     livePreview: 'https://github.com',
   },
 ]
@@ -49,23 +49,26 @@ const projects = [
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
+  'Python',
+  'JavaScript',
+  'Django',
+  'React',
+  'ExpressJS',
+  'Node.js',
   'HTML',
   'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'MongoDB',
+  'PostgreSQL',
+  'Google Cloud Platform',
+  'Microsoft Azure',
+  'RESTful APIs',
   'Git',
   'CI/CD',
-  'Jest',
-  'Enzyme',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'mjschwartz515@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
